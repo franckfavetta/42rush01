ef3a667229717669c424f4bb48068e313f4bfeb9
Franck Favetta <ffavetta@z4r10p7.42lyon.fr>
2024-09-21 16:13:11 +0200

    apres discussion ensemble - erreur si pas de solution _2

--- franck/v1/rush01.c
+++ franck/v1/rush01.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// Fills in index for further checking of one row
+// row key = base 5 number of the row
 void	fill_ok(int (*okv)[2])
 {
 	char	*cb;
@@ -14,26 +16,23 @@ void	fill_ok(int (*okv)[2])
 	i = -1;
 	while (++i < 12)
 	{
-		okv[(cb[i * 5] - '0') * 64 + (cb[i * 5 + 1] - '0') * 16 + (cb[i * 5 + 2
-			] - '0') * 4 + cb[i * 5 + 3] - '0'][0] = lft[i * 2] - '0';
-		okv[(cb[i * 5] - '0') * 64 + (cb[i * 5 + 1] - '0') * 16 + (cb[i * 5 + 2
-			] - '0') * 4 + cb[i * 5 + 3] - '0'][1] = rgt[i * 2] - '0';
+		okv[(cb[i * 5] - '0') * 125 + (cb[i * 5 + 1] - '0') * 25 + (cb[i * 5 + 2
+			] - '0') * 5 + cb[i * 5 + 3] - '0'][0] = lft[i * 2] - '0';
+		okv[(cb[i * 5] - '0') * 125 + (cb[i * 5 + 1] - '0') * 25 + (cb[i * 5 + 2
+			] - '0') * 5 + cb[i * 5 + 3] - '0'][1] = rgt[i * 2] - '0';
 	}
 	cb = "3124 3142 3214 3241 3412 3421 4123 4132 4213 4231 4312 4321 ";
 	i = -1;
 	while (++i < 12)
 	{
-		okv[(cb[i * 5] - '0') * 64 + (cb[i * 5 + 1] - '0') * 16 + (cb[i * 5 + 2
-			] - '0') * 4 + cb[i * 5 + 3] - '0'][0] = lft[i * 2 + 24] - '0';
-		okv[(cb[i * 5] - '0') * 64 + (cb[i * 5 + 1] - '0') * 16 + (cb[i * 5 + 2
-			] - '0') * 4 + cb[i * 5 + 3] - '0'][1] = rgt[i * 2 + 24] - '0';
+		okv[(cb[i * 5] - '0') * 125 + (cb[i * 5 + 1] - '0') * 25 + (cb[i * 5 + 2
+			] - '0') * 5 + cb[i * 5 + 3] - '0'][0] = lft[i * 2 + 24] - '0';
+		okv[(cb[i * 5] - '0') * 125 + (cb[i * 5 + 1] - '0') * 25 + (cb[i * 5 + 2
+			] - '0') * 5 + cb[i * 5 + 3] - '0'][1] = rgt[i * 2 + 24] - '0';
 	}
 }
 
-//Return error codes:
-// 1) No arguments or too many
-// 2) Argument string too long or too short
-// 3) View numbers error
+// Check prog param for errors & fill in 2D tab with expected view numbers
 int	param(int argc, char **argv, int *views)
 {
 	char	*p;
@@ -58,16 +57,6 @@ int	param(int argc, char **argv, int *views)
 	return (0);
 }
 
-void	all_comb(int *views, int (*okv)[2])
-{
-	unsigned int	c;
-
-	c = 0;
-	while (c <= 4294967295)
-	{
-		c++;
-	}
-}
 void	put_map(int *map)
 {
 	int	col;
@@ -87,19 +76,33 @@ void	put_map(int *map)
 	}
 }
 
-int	chk(int *row, int lft, int rgt, int (*okv)[2])
+// Checks if expected lft & rgh match this row thanks to index okv 
+int	chk_row(int *row, int lft, int rgt, int (*okv)[2])
 {
-	int	idcomb;
+	int	key;
 
 	if(row[0] == 0 || row[1] == 0 || row[2] == 0 || row[3] == 0)
 		return (1);
-	idcomb = row[0] * 64 + row[1] * 16 + row[2] * 4 + row[3];
-	if(okv[idcomb][0] != lft || okv[idcomb][1] != rgt)
+	key = row[0] * 125 + row[1] * 25 + row[2] * 5 + row[3];
+	if(okv[key][0] != lft || okv[key][1] != rgt)
 		return (0);
 	return (1);
 }
 
-//maptr = transposed map -> for easier checking
+int	chk_map(int *map, int *maptr, int *views, int (*okv)[2])
+{
+	return (chk_row(map + 0, views[8], views[12], okv) &&
+		chk_row(map + 4, views[9], views[13], okv) &&
+		chk_row(map + 8, views[10], views[14], okv) &&
+		chk_row(map + 12, views[11], views[15], okv) &&
+		chk_row(maptr + 0, views[0], views[4], okv) &&
+		chk_row(maptr + 4, views[1], views[5], okv) &&
+		chk_row(maptr + 8, views[2], views[6], okv) &&
+		chk_row(maptr + 12, views[3], views[7], okv));
+}
+
+//Sets recursively a new value, then checks the map
+//maptr = transposed map -> for easier checking (only row checking)
 int	set(int *vw, int (*okv)[2], int *map, int pos)
 {
 	int	i;
@@ -107,9 +110,7 @@ int	set(int *vw, int (*okv)[2], int *map, int pos)
 
 	if (pos == 16)
 	{
-		write(1, "found\n", 6);
 		put_map(map);
-		write(1, "found\n", 6);
 		return (1);
 	}
 	else
@@ -121,29 +122,53 @@ int	set(int *vw, int (*okv)[2], int *map, int pos)
 		while (++i < 5)
 		{
 			map[pos] = i;
-			if (chk(map + 0, vw[8], vw[12], okv) && chk(map + 4, vw[9], vw[13], okv) &&
-				chk(map + 8, vw[10], vw[14], okv) && chk(map + 12, vw[11], vw[15], okv) &&
-				chk(maptr + 0, vw[0], vw[4], okv) && chk(maptr + 4, vw[1], vw[5], okv) &&
-				chk(maptr + 8, vw[2], vw[6], okv) && chk(maptr + 12, vw[3], vw[7], okv))
-				set(vw, okv, map, pos + 1);
+			if (chk_map(map, maptr, vw, okv))
+				if (set(vw, okv, map, pos + 1))
+					return (1);
 		}
-		/*
-		*/
+		map[pos] = 0;
 	}
 	return (0);
 }
 
-//views tab order:
-//  0)col1up     1)col2up    2)col3up      3)col4up 
-//  4)col1down   5)col2down  6)col3down    7)col4down 
-//  8)row1left   9)row2left  10)row3left  11)row4left
-// 12)row1right 13)row2right 14)row3right 15)row4right
+// Skycraper Puzzle - Piscine42-2024/9
+//   - Method: 
+//      - A) Check incoming parameters
+//           Return error codes:
+//              Err1) No arguments or too many
+//              Err2) Argument string too long or too short
+//              Err3) Error in view numbers
+//              Err4) No solution found
+//      - B) Create a 2D tab index for expected view numbers 
+//           (extracted from parameters)
+//             row key -> value left 
+//           & row key -> value right
+//                Views tab order:
+//                   0)col1up     1)col2up    2)col3up      3)col4up 
+//                   4)col1down   5)col2down  6)col3down    7)col4down 
+//                   8)row1left   9)row2left  10)row3left  11)row4left
+//                  12)row1right 13)row2right 14)row3right 15)row4right
+//      - C) Browse the map recursively placing one new value & incrementing it
+//      - D) Check every time every row & every column
+//             (checking factorized by transposing the map matrix)
+//   - How to use:
+//        > ./rush-01 "col1up col2up col3up col4up col1down col2down col3down 
+//        col4down row1left row2left row3left row4left row1right row2right ro
+//        w3right row4right"
+//   - Example:
+//        > ./rush-01 "4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2"
+//     Output:
+//        1 2 3 4
+//        2 3 4 1
+//        3 4 1 2
+//        4 1 2 3
 int	main(int argc, char **argv)
 {
 	int	views[16];
 	int	retp;
-	int	okviews[314][2];
+	int	okviews[1000][2];
 	int	map[16];
+
 	retp = param(argc, argv, views);
 	if (retp == 0)
 	{
@@ -151,7 +176,9 @@ int	main(int argc, char **argv)
 		retp = 0;
 		while (retp < 16)
 			map[retp++] = 0;
-		set(views, okviews, map, 0); 
+		if (!set(views, okviews, map, 0))
+			write(1, "Error\n", 6);
+			return (4);
 	}
 	else
 	{
@@ -161,88 +188,3 @@ int	main(int argc, char **argv)
 	return (0);
 }
 
-/*
-		int i;
-		//for (i=0; i<16; i++)
-		//	printf("%d  ", views[i]); 
-		//write(1, "ici", 3);
-		for (i=0; i<314; i++)
-			okviews[i][0] = 0; 
-		for (i=0; i<314; i++)
-			okviews[i][1] = 0; 
-
-		for (i=0; i<314; i++)
-			if (okviews[i][0] != 0)
-				printf("%d %d %d\n", okviews[i][0], i, okviews[i][1]); 
-
-
-		map[3] = 7;
-		map[6] = 9;
-		map[8] = 2;
-		put_map(map);
-
-
-
-1234
-1243
-1324
-1342
-1423
-1432
-2134
-2143
-2314
-2341
-2413
-2431
-3124
-3142
-3214
-3241
-3412
-3421
-4123
-4132
-4213
-4231
-4312
-4321
-
-4 1234 1
-3 1243 2
-3 1324 1
-3 1342 2
-2 1423 2
-2 1432 3
-3 2134 1
-2 2143 2
-3 2314 1
-3 2341 2
-2 2413 2
-2 2431 3
-2 3124 1
-2 3142 2
-2 3214 1
-2 3241 2
-2 3412 2
-2 3421 3
-1 4123 2
-1 4132 3
-1 4213 2
-1 4231 3
-1 4312 3
-1 4321 4
-
-
-0 0 0 7 
-0 0 9 0 
-0 0 0 0 
-2 0 0 0
-
-0 0 0 2 
-0 0 0 0 
-0 9 0 0 
-7 0 0 0
-
-
-*/
